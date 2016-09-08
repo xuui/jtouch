87fbdc5d79111c8e76b02973d4f52542b8070cd3
Xu.hel <xu.hel@outlook.com>
2016-09-08 16:14:48 +0800

    flex box
    Signed-off-by: Xu.hel <xu.hel@outlook.com>

---
+++ resources/app.js
@@ -0,0 +1,14 @@
+$(function(){
+'use strict';
+//var jQT=new $.jQT({preloadImages:[]});
+
+console.log('xuui touch init.');
+/*$(document).ajaxStart(function(){
+  $('.ajaxloader').show();
+});*/
+/*$(document).ajaxComplete(function(){
+  $('.ajaxloader').hide();
+});*/
+//jQT.goTo('#home');
+
+});

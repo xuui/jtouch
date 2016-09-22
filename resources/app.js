ab8751826a8a16709ae53218960d5a32b5c2e48b
Xu.hel <xu.hel@outlook.com>
2016-09-22 17:10:55 +0800

    splash slider 闪屏滑动组件
    Signed-off-by: Xu.hel <xu.hel@outlook.com>

--- resources/app.js
+++ resources/app.js
@@ -2,6 +2,16 @@ $(function(){
 'use strict';
 //var jQT=new $.jQT({preloadImages:[]});
 
+$('#splash_slider').flexslider({
+  animation:"slide",
+  //animationLoop:false,
+  directionNav:false,
+  slideshow:false
+  ,end:function(){
+    console.log('Splash slider End.');
+  }
+});
+  
 console.log('xuui touch init.');
 /*$(document).ajaxStart(function(){
   $('.ajaxloader').show();

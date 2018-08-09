75d2f80318ae9f55ef0e9a4a0187ac8e089abeb9
Xuhel <xu.hel@outlook.com>
2018-08-09 17:03:25 +0800

    add  tab panel

--- resources/xutabs.js
+++ resources/xutabs.js
@@ -1,5 +1,4 @@
 //xu.Tabs.
-();
 var tabPanel,tabTit;
 
 (function(jQuery){
@@ -7,7 +6,7 @@ $=jQuery.noConflict();
 $(document).ready(function(){
   $('.tabTit a').click(function(){
     $(this).addClass('current').siblings().removeClass();
-    $('.tabPanel > ul').eq($('.tabTit a').index(this)).addClass('current').show().siblings().hide();
+    $('.tabPanel > .tabpanel').eq($('.tabTit a').index(this)).addClass('current').show().siblings().hide();
   });
 });
 })(jQuery);

ba3a1ace6ca0998aa35cabcc4fe97abd20ff50a6
Xuhel <xu.hel@outlook.com>
2018-08-14 16:13:28 +0800

    data-tab=panel

--- resources/xutabs.js
+++ resources/xutabs.js
@@ -6,7 +6,7 @@ $=jQuery.noConflict();
 $(document).ready(function(){
   $('.tabNav a').click(function(){
     $(this).addClass('current').siblings().removeClass();
-    $('.tabContent > .tabPanel').eq($('.tabNav a').index(this)).addClass('current').show().siblings().hide();
+    $('.tabContent > [data-tab=panel]').eq($('.tabNav a').index(this)).addClass('current').show().siblings().hide();
   });
 });
 })(jQuery);

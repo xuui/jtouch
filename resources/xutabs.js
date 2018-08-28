1b0a06c0d786ce2f3429be083be6444ca27d9d19
Xuhel <xu.hel@outlook.com>
2018-08-28 11:01:50 +0800

    scrollTop(0)

--- resources/xutabs.js
+++ resources/xutabs.js
@@ -7,6 +7,7 @@ $(document).ready(function(){
   $('[data-tab=nav] a').click(function(){
     $(this).addClass('current').siblings().removeClass();
     $('[data-tab=content] > [data-tab=panel]').eq($('.tabNav a').index(this)).addClass('current').show().siblings().hide();
+    $('[data-tab=content]').scrollTop(0);
   });
 });
 })(jQuery);

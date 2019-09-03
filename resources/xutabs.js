82f9f313d2356de00ee200860143377c811e294f
Xuhel <xu.hel@outlook.com>
2019-09-03 13:57:48 +0800

    tabbar

--- resources/xutabs.js
+++ resources/xutabs.js
@@ -3,11 +3,9 @@ var tabPanel,tabTit;
 
 //(function(jQuery){
 //$=jQuery.noConflict();
-//$(document).ready(function(){
-  $('[data-tab=nav] a').click(function(){
-    $(this).addClass('current').siblings().removeClass();
-    $('[data-tab=content] > [data-tab=panel]').eq($('.tabNav a').index(this)).addClass('current').show().siblings().hide();
-    $('[data-tab=content]').scrollTop(0);
-  });
-//});
+$('[data-tab=nav] a').click(function(){
+  $(this).addClass('current').siblings().removeClass();
+  $('[data-tab=content] > [data-tab=panel]').eq($('.tabNav a').index(this)).addClass('current').show(200).siblings().hide(200);
+  $('[data-tab=content]').scrollTop(0);
+});
 //})(jQuery);

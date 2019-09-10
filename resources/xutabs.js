c753e665fa02140b81e2022f5bb4806e8740262f
Xuhel <xu.hel@outlook.com>
2019-09-10 17:39:20 +0800

    Update xutabs.js

--- resources/xutabs.js
+++ resources/xutabs.js
@@ -5,7 +5,7 @@ var tabPanel,tabTit;
 //$=jQuery.noConflict();
 $('[data-tab=nav] a').click(function(){
   $(this).addClass('current').siblings().removeClass();
-  $('[data-tab=content] > [data-tab=panel]').eq($('.tabNav a').index(this)).addClass('current').show(200).siblings().hide(200);
+  $('[data-tab=content] > [data-tab=panel]').eq($('.tabNav a').index(this)).addClass('current').show(100).siblings().hide(100);
   $('[data-tab=content]').scrollTop(0);
 });
 //})(jQuery);

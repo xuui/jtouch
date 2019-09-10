726dd9fe7003416022d1f1a556eadfbc7b21a520
Xuhel <xu.hel@outlook.com>
2019-09-10 17:32:31 +0800

    add Material Icon

--- _resources/app.js
+++ _resources/app.js
@@ -22,25 +22,6 @@ $('#splash').bind('pageAnimationEnd',function(e,info){
   }
 });
 
-$('#MaterialIcon').bind('pageAnimationEnd',function(e,info){
-  if(info.direction=='in'){
-    if($('#MaterialIcon #icons-cont').html()==''){
-      $('#MaterialIcon #icons-cont').html('<li class="xu-u">Loadind Material Icon...</li>');
-      $.getJSON('resources/fonts/mdicons.json',function(data){
-        var mdicons=data.icons,icons_group='';
-        $('#MaterialIcon #icons-cont').html('');
-        for(var i=0;i<mdicons.length;i++){
-          if(icons_group!=mdicons[i].group_id){
-            icons_group=mdicons[i].group_id;
-            $('#MaterialIcon #icons-cont').append('<li class="xu-u">'+mdicons[i].group_id+'</li>');
-          }
-          $('#MaterialIcon #icons-cont').append('<li class="xu-u-3 '+mdicons[i].group_id+'"><a href="javascript:;"><i class="material-icons">'+mdicons[i].ligature+'</i> <span class="caption">'+mdicons[i].ligature+'</span></a></li>');// &#x'+mdicons[i].codepoint+';
-        }
-      });
-    }
-  }else{}
-});
-
 $('#animdemo').bind('pageAnimationStart',function(e,info){
   $(this).find('h2.vtitle').html($(this).data('referrer').data('title'));
 });

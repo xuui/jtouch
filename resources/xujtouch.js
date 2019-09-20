da4ab9e852446667bddb6aa0eb1cb50980da0c61
Xuhel <xu.hel@outlook.com>
2019-09-20 16:19:10 +0800

    Update xujtouch.js

--- resources/xujtouch.js
+++ resources/xujtouch.js
@@ -1,12 +1,16 @@
 //xu.Touch.
+(function(){
+  'use strict';
+  // TODO add service worker code here
+  if('serviceWorker' in navigator){
+    navigator.serviceWorker.register('./service-worker.js')
+      .then(function(){ console.log('Service Worker Registered'); });
+  }
+})();
+
 //(function(jQuery){
 //$=jQuery.noConflict();
 // TODO add service worker code here
-if('serviceWorker' in navigator){
-  navigator.serviceWorker.register('../service-worker.js').then(function(){
-    console.log('Service Worker Registered');
-  });
-}
 
 var jQT=new $.jQT({});
 //Launch image.
@@ -47,3 +51,51 @@ $('#MaterialIcon').bind('pageAnimationEnd',function(e,info){
 });
 
 //})(jQuery);
+
+
+
+
+// beforeinstallprompt //
+window.addEventListener('beforeinstallprompt', function(e) {
+  // beforeinstallprompt Event fired
+
+  // e.userChoice will return a Promise. 
+  // For more details read: https://developers.google.com/web/fundamentals/getting-started/primers/promises
+  e.userChoice.then(function(choiceResult) {
+    console.log(choiceResult.outcome);
+    if(choiceResult.outcome == 'dismissed') {
+      console.log('User cancelled home screen install');
+    } else {
+      console.log('User added to home screen');
+    }
+  });
+});
+
+/*
+var deferredPrompt;
+window.addEventListener('beforeinstallprompt', function(e) {
+  console.log('beforeinstallprompt Event fired');
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  return false;
+});
+btnSave.addEventListener('click', function() {
+  if(deferredPrompt !== undefined) {
+    // The user has had a postive interaction with our app and Chrome
+    // has tried to prompt previously, so let's show the prompt.
+    deferredPrompt.prompt();
+    // Follow what the user has done with the prompt.
+    deferredPrompt.userChoice.then(function(choiceResult) {
+      console.log(choiceResult.outcome);
+      if(choiceResult.outcome == 'dismissed') {
+        console.log('User cancelled home screen install');
+      } else {
+        console.log('User added to home screen');
+      }
+      // We no longer need the prompt.  Clear it up.
+      deferredPrompt = null;
+    });
+  }
+});
+*/

32473f1aca00f5d197eb573b095b1cf36439761b
Xuhel <xu.hel@outlook.com>
2019-09-20 15:35:56 +0800

    TileImage

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@ var filesToCache = [ // app shell 需要的缓存的文件列表
   'resources/icons/icon-256x256.png',
 ];
 
-self.addEventListener('install', function(e) { // 注册 Service Worker
+self.addEventListener('install',function(e){ // 注册 Service Worker
   console.log('[ServiceWorker] Install');
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
@@ -47,7 +47,7 @@ self.addEventListener('install', function(e) { // 注册 Service Worker
   );
 });
 
-self.addEventListener('activate', function(e) { // 激活外壳(app shell)
+self.addEventListener('activate',function(e){ // 激活外壳(app shell)
   console.log('[ServiceWorker] Activate');
   e.waitUntil(
     caches.keys().then(function(keyList) {
@@ -60,4 +60,37 @@ self.addEventListener('activate', function(e) { // 激活外壳(app shell)
     })
   );
   return self.clients.claim();
+});
+
+self.addEventListener('fetch',function(e){ //清理应用程序的外壳(app shell)缓存，并不会删除数据缓存。
+  console.log('[Service Worker] Fetch',e.request.url);
+  /*var dataUrl = 'https://query.yahooapis.com/v1/public/yql';
+  if (e.request.url.indexOf(dataUrl) > -1) {
+    /*
+     * When the request URL contains dataUrl, the app is asking for fresh
+     * weather data. In this case, the service worker always goes to the
+     * network and then caches the response. This is called the "Cache then
+     * network" strategy:
+     * https://jakearchibald.com/2014/offline-cookbook/#cache-then-network
+     *//*
+    e.respondWith(
+      caches.open(dataCacheName).then(function(cache) {
+        return fetch(e.request).then(function(response){
+          cache.put(e.request.url, response.clone());
+          return response;
+        });
+      })
+    );
+  } else {
+    /*
+     * The app is asking for app shell files. In this scenario the app uses the
+     * "Cache, falling back to the network" offline strategy:
+     * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
+     *//*
+    e.respondWith(
+      caches.match(e.request).then(function(response) {
+        return response || fetch(e.request);
+      })
+    );
+  }*/
 });
